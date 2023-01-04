58fc4010de53ce7af1ac2e32b8881bc0196e70fa
Young Ouk Na <dyang34@naver.com>
2023-01-04 14:28:54 +0900

    Delete CmsConfig.php
    important info

--- classes/cms/CmsConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-class CmsConfig {
-    static $mysql_host = "localhost";
-    static $mysql_user = "root";
-    static $mysql_password = "1dkdlvptelql.";
-    static $mysql_database = "ts9010";
-}
-?>
